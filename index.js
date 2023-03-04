22068c9c5aa8c11874f84187b65c1b92a689fbb7
farida ndiaye <faridandiaye60@gmail.com>
2023-03-04 13:26:06 -0500

    Uses Loop Function

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const message = [];
+
+function writeCards(name,event){
+    const newMessage = [...message];
+    for (let i= 0; i < name.length; i++){
+    newMessage.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return newMessage;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number){
+    for (let i = 0; i <11; i++ ){
+
+        console.log(number--);
+        
+    }
+    
+}
